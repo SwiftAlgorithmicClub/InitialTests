e14aa84da6e54ee1f34d0e08b0118b7e47fbfca8
Oleksii Tiurenkov <oleksii_tiurenkov@epam.com>
2017-02-03 12:38:53 +0200

    Initial seed

---
+++ inititalTests/inititalTests/FizzBuzzTest.swift
@@ -0,0 +1,29 @@
+//
+//  FeedBuzzTest.swift
+//  inititalTests
+//
+//  Created by Oleksii Tiurenkov on 2/3/17.
+//  Copyright © 2017 Swift clUb. All rights reserved.
+//
+
+import Foundation
+
+
+/*
+ The program should iterate over all integers (inclusive) from 1 to N.
+ 
+ * For integers that are evenly divisible by three, return "Hoppity"
+ * For integers that are evenly divisible by four, return "HopHop"
+ * For integers that are evenly divisible by both three and four, print just "Hop"
+ * For all other integers, return 0.
+ */
+
+
+
+public class FaceBookVariation
+{
+    func hoppityHop(_ N: Int) -> [String]
+    {
+        return []
+    }
+}
---
+++ inititalTests/inititalTests/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  inititalTests
+//
+//  Created by Oleksii Tiurenkov on 2/3/17.
+//  Copyright © 2017 Swift clUb. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+print("Hello, World!")
+
